7f047eaddb99c1c24cae4728569c8d9dab179510
Sylvain Ramshaw <sylvain.ramshaw@gmail.com>
2017-07-01 18:54:06 -0500

    segregate sctipts, includes and examples

--- examples/pythagorian/pythagorian.sh
+++ examples/pythagorian/pythagorian.sh
@@ -8,12 +8,12 @@
 #g++ -std=c++14 -I . -O0 -fno-inline -fdata-sections -ffunction-sections -g pythagorian.cpp -Wunused-result -o main1 -Wl,--gc-sections 2>&1 | grep error 
 #size main1 && ./main1 | c++filt -t
 
-rm pythagorian
+rm pythagorian.exe
 echo '\n\n\n\n////////////////////  START GENERATION pythagorian //////////////////' 
 #g++ -std=c++14 -I . -Os -fdata-sections -ffunction-sections pythagorian.t-rx.out.cpp -Wunused-result -o pythagorian -Wl,--gc-sections 2>&1 | grep error 
-g++ -std=c++14 -I . -Os -fdata-sections -ffunction-sections pythagorian.t-rx.out.cpp -Wunused-result -o pythagorian -Wl,--gc-sections 2>&1 | grep error 
+g++ -std=c++14 -I . -Os -fdata-sections -ffunction-sections pythagorian.t-rx.out.cpp -Wunused-result -o pythagorian.exe -Wl,--gc-sections 2>&1 | grep error 
 #g++ -S -masm=intel -std=c++14 -I . -Os -fdata-sections -ffunction-sections pythagorian.t-rx.out.cpp -Wunused-result -o pythagorian.s -Wl,--gc-sections 2>&1 | grep error 
-size pythagorian && ./pythagorian | c++filt -t
+size pythagorian.exe && ./pythagorian.exe | c++filt -t
 
 #watch -n 1 "g++ -std=c++14 -O3 -fdata-sections -ffunction-sections pythagorian.cpp -o main1 -Wl,--gc-sections && size main1 && ./main1"
 #watch -n 1 "g++ -std=c++14 -Os pythagorian.cpp -o main1 -Wl,--gc-sections && size main1 && ./main1"
---
+++ scripts/tirnsr_rx.py
@@ -0,0 +1,13 @@
+import os,sys
+from t_rx_file import *
+
+for root,dirs,files in os.walk(sys.argv[1]):
+    for f in files:
+        if f.endswith('.t-rx.in.cpp'):
+            print f
+            generateCodeForOneFile(os.path.join(root,f))
+
+print('######-- DONE --####')
+print('######-- DONE --####')
+
+
