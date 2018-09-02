9adaed3284bb311bdfc0c9b51304be9d88255aaf
Sylvain Ramshaw <sylvain.ramshaw@gmail.com>
2018-09-01 21:25:05 -0500

    add tools to the path for compilation on Windows, to be sourced in bash

---
+++ setup.sh
@@ -0,0 +1,5 @@
+PYTHON27_PATH=/C/python27/python-2.7.13.amd64
+CLANG_FORMAT_PATH=/c/Users/sylva/.vscode/extensions/ms-vscode.cpptools-0.18.1/LLVM/bin
+GPP_PATH="/c/Program Files (x86)/mingw-w64/i686-8.1.0-posix-dwarf-rt_v6-rev0/mingw32/bin"
+PATH=$PYTHON27_PATH:$CLANG_FORMAT_PATH:$GPP_PATH:$PATH
+
