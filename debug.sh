1c2d74f09e1d6d7762327c41613ff5a9b3e060f8
Sylvain Ramshaw <sylvain.ramshaw@gmail.com>
2025-01-16 23:08:02 -0600

    add devcontainer defs

---
+++ debug.sh
@@ -0,0 +1,1 @@
+python3 -s ./scripts/tirnsr_rx.py . && sh -c 'cd ./examples/pythagorian/ && ./debug.sh' && sh -c 'cd ./examples/showcase/ && ./debug.sh' 
---
+++ examples/showcase/debug.sh
@@ -0,0 +1,4 @@
+echo '\n\n\n\n////////////////////  COMPILING pythagorian //////////////////' 
+rm showcase-debug
+g++ -std=c++14 -I . pythagorian.t-rx.out.cpp -o showcase-debug 
+
