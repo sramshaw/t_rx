6250fcf633e0fce9eeef390c070c3a9455e1ba09
Sylvain Ramshaw <sylvain.ramshaw@gmail.com>
2025-01-17 06:52:43 +0000

    fix devcontainers approach

--- examples/pythagorian/debug.sh
+++ examples/pythagorian/debug.sh
@@ -1,4 +1,4 @@
 echo '\n\n\n\n////////////////////  COMPILING pythagorian //////////////////' 
 rm pythagorian-debug
-g++ -std=c++14 -I . pythagorian.t-rx.out.cpp -o pythagorian-debug 
+g++ -g -std=c++14 -I . pythagorian.t-rx.out.cpp -o pythagorian-debug 
 
