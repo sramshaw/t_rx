3cd4aeded2c9204d23c5b8744f4ac536410edbd9
Sylvain Ramshaw <sylvain.ramshaw@gmail.com>
2025-01-17 16:20:48 +0000

    cleanup

--- dockerfile
+++
@@ -1,6 +0,0 @@
-FROM ubuntu
-
-RUN apt-get update
-RUN apt-get install -y python3 g++ clang-format fswatch
-
-WORKDIR /app
--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-python3 -s ./scripts/tirnsr_rx.py . && sh -c 'cd ./examples/pythagorian/ && ./pythagorian.sh' && sh -c 'cd ./examples/showcase/ && ./showcase.sh' 
+python3 -s ./scripts/tirnsr_rx.py . &&
+sh -c 'cd ./examples/pythagorian/ && ./build.sh' &&
+sh -c 'cd ./examples/showcase/ && ./build.sh' 
