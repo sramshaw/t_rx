d6c564cca0472d05633fe4843ae5dc3760f7b9f6
Sylvain Ramshaw <sylvain.ramshaw@gmail.com>
2025-01-21 18:25:08 +0000

    cleanup

--- debug.sh
+++
@@ -1,3 +0,0 @@
-python3 -s ./scripts/tirnsr_rx.py . && \
-(cd ./examples/pythagorian/ && ./debug.sh) && \
-(cd ./examples/showcase/ && ./debug.sh)
--- examples/pythagorian/debug.sh
+++
@@ -1,4 +0,0 @@
-echo '\n\n\n\n////////////////////  COMPILING pythagorian //////////////////' 
-rm pythagorian-debug
-g++ -g -std=c++14 -I . pythagorian.t-rx.out.cpp -o pythagorian-debug 
-
